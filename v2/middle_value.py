7720437e223fe3964b5ca1171984f29114d4b193
Kenan Batur <30412755+Kenanbatur23@users.noreply.github.com>
2024-07-11 16:21:45 +0300

    eski  veriler

--- v2/middle_value.py
+++ v2/middle_value.py
@@ -13,17 +13,18 @@ class LightningRiskCalculator_middle_values:
         self.C_D = min.c_d_belirle()
         self.N_D = self.N_G*self.A_D[0]*self.C_D
         return self.N_D
-    def p_a_belirleme(self):
+    def p_a_belirle(self):
         self.P_A = min.p_ta_belirle()*min.p_b_belirle()
         return self.P_A
     def l_a_belirle(self):
         self.L_A = min.r_t_belirle()*min.l_t_belirle()*min.n_z_bölü_n_t_belirle()*min.t_z_bölü_8760_belirle()
+        return self.L_A
     #####################################################################################3
     def n_m_belirle(self):
         self.A_M = min.a_m_belirle()
         self.N_M = self.N_G* self.A_M
         return self.N_M    
 x = LightningRiskCalculator_middle_values()
-print(x.n_d_belirle())
-print(x.n_m_belirle())
+print(x.n_d_belirle(),x.n_d_belirle(),x.p_a_belirle(),x.l_a_belirle())
+
 
