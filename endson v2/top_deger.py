3c4b6e7f15b4b5c87f21016da97ee7a07b38f659
Mert Sivri <Cyrlex@users.noreply.github.com>
2024-07-19 10:30:57 +0300

    r1-r2-r3-r4 bitti

--- endson v2/top_deger.py
+++ endson v2/top_deger.py
@@ -37,8 +37,22 @@ class LightningRiskCalculator_top_values:
         self.N_I = mid.n_ı_belirle()
         self.P_Z = mid.p_z_belrile()
 
+        self.R_B_3 = None
+        self.L_B_3 = self.L_V_3 =  mid.l_bv_3_belirle()
 
-    
+        self.R_A_4 = None
+        self.L_A_4 = self.L_U_4 = mid.l_au_4_belirle()
+        self.R_B_4 = None
+        self.L_B_4 = self.L_V_4 = mid.l_bv_4_belirle()
+        self.R_C_4 = None
+        self.L_C_4 = self.L_M_4 = self.L_W_4 = self.L_Z_4 = mid.l_cmwz_4_belirle()
+        self.R_M_4 = None
+        self.R_U_4 = None
+        self.R_V_4 = None
+        self.R_W_4 = None
+        self.R_Z_4 = None
+
+
     def r_a_1_belirle(self):
         self.R_A_1 = self.N_D*self.P_A*self.L_A
         return self.R_A_1
@@ -82,6 +96,42 @@ class LightningRiskCalculator_top_values:
         self.R_Z_2 = self.N_I*self.P_Z*self.L_Z_2
         return self.R_Z_2
     
+    #r3 icin r'ler
+
+    def r_b_3_belirle(self):
+        self.R_B_3 = self.N_D*self.P_B*self.L_B_2
+        return self.R_B_3
+    def r_v_3_belirle(self):
+        self.R_V_3 = (self.N_L + self.N_DJ)*self.P_V*self.L_V_3
+        return self.R_V_3
+    
+    #r4 icin r'ler
+
+    def r_a_4_belirle(self):
+        self.R_A_4 = self.N_D*self.P_A*self.L_A_4
+        return self.R_A_4
+    def r_b_4_belirle(self):
+        self.R_B_4 = self.N_D*self.P_B*self.L_B_4
+        return self.R_B_4
+    def r_c_4_belirle(self):
+        self.R_C_4 = self.N_D*self.P_C*self.L_C_4
+        return self.R_C_4
+    def r_m_4_belirle(self):
+        self.R_M_4 = self.N_M*self.P_M*self.L_M_4
+        return self.R_M_4
+    def r_u_4_belirle(self):
+        self.R_U_4 = (self.N_L + self.N_DJ)*self.P_U*self.L_U_4
+        return self.R_U_4
+    def r_v_4_belirle(self):
+        self.R_V_4 = (self.N_L + self.N_DJ)*self.P_V*self.L_V_4
+        return self.R_V_4
+    def r_w_4_belirle(self):
+        self.R_W_4 = (self.N_L + self.N_DJ)*self.P_W*self.L_W_4
+        return self.R_W_4
+    def r_z_4_belirle(self):
+        self.R_Z_4 = self.N_I*self.P_Z*self.L_Z_4
+        return self.R_Z_4
+    
         
 x = LightningRiskCalculator_top_values()
 print(x.r_a_1_belirle())
@@ -92,14 +142,25 @@ print(x.r_u_1_belirle())
 print(x.r_v_1_belirle())
 print(x.r_w_1_belirle())
 
-
 print(x.r_b_2_belirle())
 print(x.r_c_2_belirle())
 print(x.r_m_2_belirle())
 print(x.r_v_2_belirle())
 print(x.r_w_2_belirle())
+print(x.r_z_2_belirle())
+
+print(x.r_b_3_belirle())
+print(x.r_v_3_belirle())
 
 
+print(x.r_a_4_belirle())
+print(x.r_b_4_belirle())
+print(x.r_c_4_belirle())
+print(x.r_m_4_belirle())
+print(x.r_u_4_belirle())
+print(x.r_v_4_belirle())
+print(x.r_w_4_belirle())
+print(x.r_z_4_belirle())
 
 '''
 ⠁⠀⠂⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
